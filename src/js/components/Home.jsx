c804204b513e261d4c509afe4aa84641a0a1c4ba
RdeRausseo <amrausseo20@gmail.com>
2025-07-23 02:00:35 +0000

    solo falta establecer el focus

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import Color from "./Color";
 import Palito from "./Palito";
 import Contenedor from "./Contenedor";
+import useState from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+  const [focus, setFocus] = useState("Red");
   return (
     <>
       <Palito />
       <Contenedor>
-        <Color color="Red" />
-        <Color color="Yellow" />
-        <Color color="Green" />
+        <Color onClick={() => setFocus("Red")} color="Red" />
+        <Color onClick={() => setFocus("Yellow")} color="Yellow" />
+        <Color onClick={() => setFocus("Green")} color="Green" />
       </Contenedor>
     </>
   );
