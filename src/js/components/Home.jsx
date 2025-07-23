688a51185a0415356eb0a279be76b0a4be5ee515
RdeRausseo <amrausseo20@gmail.com>
2025-07-23 00:54:27 +0000

    agregando componente palito que se va a renderizar para nuestro traffic light

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import Color from "./Color";
-
+import Palito from "./Palito";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div>
-			<Color color="Red"/>
-		</div>
-	)
+  const styleContenedor = {
+    width: "200px",
+    height: "400px",
+    backgroundColor: "black",
+    padding: "10px",
+  };
+
+  return (
+    <>
+      <Palito />
+      <Color color="Red" />
+      <Color color="Yellow" />
+      <Color color="Green" />
+    </>
+  );
 };
 
 export default Home;
---
+++ src/js/components/Palito.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const palito = () => {
+  const stylePalito = {
+    width: "40px",
+    height: "150px",
+    backgroundColor: "black",
+    borderRadius: "20%",
+    margin: "20px auto",
+  };
+
+  return <div style={stylePalito}></div>;
+};
+
+export default palito;
